d0bed4b1b6c3855c064d0f429b5948ff527e9f52
14thDovePin <therlaf0409@gmail.com>
2024-01-24 17:56:16 +0800

    Refactored and improved script configuration.
    - Renamed function `create_config` to `configuration`.
    - Refactored code.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """TODO LIST
 - Implement configuration file.
+    - Implement `config.ini` check system.
 - Implement file and folder management.
 - Implement sorting algorithm.
 - Implement naming system.
@@ -20,25 +21,29 @@ CONFIG_DIR = os.path.join(os.getcwd(), CONFIG_FN)
 
 def main():
     """The main function of the script."""
+    # Configure script.
+    configuration()
+
+
+def configuration():
+    """Manage the script's configuration."""
     if not os.path.exists(CONFIG_DIR):
-        create_config()
+
+        # Construct and write the config file.
+        CONFIG['CONFIGURATION'] = {
+            'manual_input_mode': 'True',
+            'default_sorting_directory': 'None',
+            'max_file_count_per_group': '20',
+            'segregate_file_types': 'False',
+            'use_date_for_grouping': 'False',
+            }
+        with open(CONFIG_DIR, 'w') as f:
+            CONFIG.write(f)
         print("Configuration File Created!")
 
-
-def create_config():
-    """Create the configuration file."""
-    # Construct configuration.
-    CONFIG['CONFIGURATION'] = {
-        'manual_input_mode': 'true',
-        'default_sorting_directory': 'None',
-        'max_file_count_per_group': '20',
-        'segregate_file_types': 'false',
-        'use_date_for_grouping': 'false',
-        }
-
-    # Write configuration.
-    with open(CONFIG_DIR, 'w') as f:
-        CONFIG.write(f)
+    # Read config file.
+    CONFIG.read(CONFIG_DIR)
+    config = CONFIG['CONFIGURATION']
 
 
 if __name__ == '__main__':
