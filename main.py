e2ba20cfaa9f73a024b97b10a3a5212b20159236
14thDovePin <therlaf0409@gmail.com>
2024-01-24 11:34:11 +0800

    Added a TODO list in `main.py`.
    - Modified `main.py`.

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+"""TODO LIST
+- Implement configuration file.
+    - Add option for manual input of root directory to sort.
+    - Option for maximum number of files per group.
+    - Option for segregating file types.
+- Implement file and folder management.
+- Implement sorting algorithm.
+- Implement naming system.
+    - Rename files based on their date and time meta data.
+    - Store the original file names using the csv data structure.
+"""
+
+
 def main():
     pass
 
